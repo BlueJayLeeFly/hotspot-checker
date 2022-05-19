33b2580c1e0d751266ea01f313a1c5925b098d86
Jay Lee <slee9111@conestogac.on.ca>
2022-05-19 01:45:01 -0400

    create grid and set counter to the grid children

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+const container = document.querySelector('.grid-container');
+
+for (let i = 0; i < 2500; i++) {
+	let counter = 100;
+	const spot = document.createElement('div');
+	spot.classList.add('spot');
+	container.appendChild(spot);
+
+	spot.addEventListener('mouseover', () => {
+		if (counter > 0) {
+			counter -= 10;
+		}
+		spot.style.backgroundColor = `hsl(269, 100%, ${counter}%)`;
+		console.log(spot.style.backgroundColor);
+	});
+}
