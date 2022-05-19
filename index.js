5c2d2fccb97fee773dd092abf069de71bfc87492
Jay Lee <slee9111@conestogac.on.ca>
2022-05-19 01:56:48 -0400

    Set up functions to customize grid

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const container = document.querySelector('.grid-container');
 
-for (let i = 0; i < 2500; i++) {
-	let counter = 100;
-	const spot = document.createElement('div');
-	spot.classList.add('spot');
-	container.appendChild(spot);
+const createSpots = (x, y) => {
+	container.style.gridTemplateColumns = `repeat(${x}, 1fr)`;
+	container.style.gridTemplateRows = `repeat(${y}, 1fr)`;
 
-	spot.addEventListener('mouseover', () => {
-		if (counter > 0) {
-			counter -= 10;
-		}
-		spot.style.backgroundColor = `hsl(269, 100%, ${counter}%)`;
-		console.log(spot.style.backgroundColor);
-	});
-}
+	for (let i = 0; i < x * y; i++) {
+		let counter = 100;
+		const spot = document.createElement('div');
+		container.appendChild(spot);
+
+		spot.addEventListener('mouseover', () => {
+			if (counter > 0) {
+				counter -= 10;
+			}
+			spot.style.backgroundColor = `hsl(269, 100%, ${counter}%)`;
+		});
+	}
+};
+
+createSpots(100, 100);
